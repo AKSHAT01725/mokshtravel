e8d6e0cafd93132710f69e70f07e0c5311ec8c11
akshat_dev <144145140+AKSHAT01725@users.noreply.github.com>
2026-08-08 19:35:02 +0530

    Refactor firebase-config.js for clarity and structure

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-// firebase-config.js
-
 import { initializeApp }        from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAnalytics }         from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 
